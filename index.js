56832d79d61315ce6082ed5c587ac5b2eb1985c2
MIcael Franco <micael.franco@aventus.io>
2026-01-19 20:29:50 +0000

    fix: slack reports and make it easy to call from jenkins

--- index.js
+++ index.js
@@ -8,101 +8,103 @@ function SlackTestReportNotification(slackUrl) {
   this.slack = SlackNotify(slackUrl);
 }
 
-SlackTestReportNotification.prototype.sendReportNotification = function(reportJsonFilePath, testName, chain) {
-
+SlackTestReportNotification.prototype.sendReportNotification = function (reportJsonFilePath, testType, chain) {
     const resolvedPath = path.resolve(__dirname, reportJsonFilePath);
     const jsonString = fs.readFileSync(resolvedPath);
     const reportConfig = JSON.parse(jsonString);
 
-    // Only sends notification if something fails
-    if (reportConfig.stats.failures === 0) return;
-
-    const alertColor = "#bd2020";
-    const resultMessage = "FAILURE";
+    const failures = reportConfig.stats.failures || 0;
+    const isFailure = failures > 0;
+    const alertColor = isFailure ? "#bd2020" : "#2eb886";
+    const resultMessage = isFailure ? "FAILURE" : "SUCCESS";
 
     const startDate = new Date(reportConfig.stats.start);
     const endDate = new Date(reportConfig.stats.end);
 
-    let stringStartDate = reportConfig.stats.start.split('T').pop().split('.')[0];
-    let stringEndDate = reportConfig.stats.end.split('T').pop().split('.')[0];
+    let stringStartDate = reportConfig.stats.start.split('T').pop().replace('Z','');
+    let stringEndDate = reportConfig.stats.end.split('T').pop().replace('Z','');
 
     const diffTime = endDate.getTime() - startDate.getTime();
 
-    var duration = new Date(null);
-    duration.setMilliseconds(diffTime + oneSecondInMiliseconds); // We add one extra second to round up the result
-    var hhmmssDateFormat = duration.toISOString().substr(11, 8);
+    let duration = new Date(null);
+    duration.setMilliseconds(diffTime);
+    let hhmmssDateFormat = duration.toISOString().substr(11, 8);
 
     this.slack.alert({
         attachments: [
+        {
+            color: alertColor,
+            blocks: [
             {
-                color: alertColor,
-                blocks: [
-                    {
-                        type: "header",
-                        text: {
-                            type: "plain_text",
-                            text: `${chain.toUpperCase()} ${testName} Test results - ${resultMessage}`,
-                        }
-                    },
-                    {
-                        type: "section",
-                        fields: [
-                            {
-                                type: "mrkdwn",
-                                text: `*Date:*\n${startDate.getDate()}/${startDate.getMonth()}/${startDate.getFullYear()}`
-                            },
-                            {
-                                type: "mrkdwn",
-                                text: `*Duration:*\n${hhmmssDateFormat}s`
-                            }
-                        ]
-                    },
-                    {
-                        type: "section",
-                        fields: [
-                            {
-                                type: "mrkdwn",
-                                text: `*Start_time:*\n${stringStartDate}`
-                            },
-                            {
-                                type: "mrkdwn",
-                                text: `*End_time:*\n${stringEndDate}`
-                            }
-                        ]
-                    }
-                ],
+                type: "header",
+                text: {
+                type: "plain_text",
+                text: `${chain.toUpperCase()} ${testType} Test results - ${resultMessage}`,
+                }
             },
             {
-                color: alertColor,
+                type: "section",
                 fields: [
-                    {   title: 'Total Test count',
-                        value: `${reportConfig.stats.tests}/${reportConfig.stats.tests} - ${getPercentage(reportConfig.stats.tests, reportConfig.stats.tests)}%`,
-                        short: true
-                    },
-                    {   title: 'Skipped',
-                        value: `${reportConfig.stats.skipped}/${reportConfig.stats.tests} - ${getPercentage(reportConfig.stats.skipped, reportConfig.stats.tests)}%`,
-                        short: true
-                    },
-                    {   title: 'Passes',
-                        value: `${reportConfig.stats.passes}/${reportConfig.stats.tests} - ${getPercentage(reportConfig.stats.passes, reportConfig.stats.tests)}%`,
-                        short: true
-                    },
-                    {   title: 'Failures',
-                        value: `${reportConfig.stats.failures}/${reportConfig.stats.tests} - ${getPercentage(reportConfig.stats.failures, reportConfig.stats.tests)}%`,
-                        short: true
-                    },
-                    {
-                        title: 'Report URL',
-                        value: `https://drive.google.com/uc?id=${reportConfig.fileId}&export=download`,
-                    }
-                ],
+                {
+                    type: "mrkdwn",
+                    text: `*Date:*\n${startDate.getDate()}/${startDate.getMonth() + 1}/${startDate.getFullYear()}`
+                },
+                {
+                    type: "mrkdwn",
+                    text: `*Duration:*\n${hhmmssDateFormat}s`
+                }
+                ]
+            },
+            {
+                type: "section",
+                fields: [
+                {
+                    type: "mrkdwn",
+                    text: `*Start_time:*\n${stringStartDate}`
+                },
+                {
+                    type: "mrkdwn",
+                    text: `*End_time:*\n${stringEndDate}`
+                }
+                ]
             }
+            ],
+        },
+        {
+            color: alertColor,
+            fields: [
+            {
+                title: 'Total Test count',
+                value: `${reportConfig.stats.tests}/${reportConfig.stats.tests} - ${getPercentage(reportConfig.stats.tests, reportConfig.stats.tests)}%`,
+                short: true
+            },
+            {
+                title: 'Skipped',
+                value: `${reportConfig.stats.skipped}/${reportConfig.stats.tests} - ${getPercentage(reportConfig.stats.skipped, reportConfig.stats.tests)}%`,
+                short: true
+            },
+            {
+                title: 'Passes',
+                value: `${reportConfig.stats.passes}/${reportConfig.stats.tests} - ${getPercentage(reportConfig.stats.passes, reportConfig.stats.tests)}%`,
+                short: true
+            },
+            {
+                title: 'Failures',
+                value: `${reportConfig.stats.failures}/${reportConfig.stats.tests} - ${getPercentage(reportConfig.stats.failures, reportConfig.stats.tests)}%`,
+                short: true
+            },
+            {
+                title: `Test Reports for ${chain}`,
+                value: `https://https://aventusdao.github.io/avn-test-reports/${testType}/${chain}/`,
+            }
+            ],
+        }
         ]
     });
 }
 
-function getPercentage(target, total){
-    const percentage = (target * 100) / total;
-    return Math.round(percentage * 100) / 100;
+function getPercentage(target, total) {
+const percentage = (target * 100) / total;
+return Math.round(percentage * 100) / 100;
 }
 
